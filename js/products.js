2adc94c9c7af15f3d924eb2924df25b47c8c9366
todobanners <ecgb2007@gmail.com>
2022-08-19 19:33:57 -0300

    Mejoras para mostrar todas las categorias

--- js/products.js
+++ js/products.js
@@ -1,28 +1,40 @@
-const URL_PRODUCTOS_AUTOS = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+let numCategory = localStorage.getItem("catID");
+if (numCategory == null){
+    let tID = setTimeout(function () {
+        window.location.href = "/categories.html";
+        window.clearTimeout(tID);		// clear time out.
+    }, 5000);
 
-function showCarCat() {
+    document.getElementById("textoP").innerHTML = `No ha seleccionado una categoría, será redirigido a la lista de <a href="/categories.html">categorias</a>`;
+}
+
+const URL_PRODUCTS = "https://japceibal.github.io/emercado-api/cats_products/" + numCategory + ".json";
+
+function showProducts() {
     let htmlContentToAppend = "";
-    fetch(URL_PRODUCTOS_AUTOS).then(resp => resp.json()).then(data => {
-        for (const car of data.products){
+    fetch(URL_PRODUCTS).then(resp => resp.json()).then(data => {
+        for (const product of data.products){
             htmlContentToAppend += `
-             <div onclick="setCatID(${car.id})" class="list-group-item list-group-item-action cursor-active">
+             <div class="list-group-item list-group-item-action cursor-active">
                  <div class="row">
                      <div class="col-3">
-                         <img src="${car.image}" alt="${car.description}" class="img-thumbnail">
+                         <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
                      </div>
                      <div class="col">
                          <div class="d-flex w-100 justify-content-between">
-                             <h4 class="mb-1">${car.name} - ${car.currency} ${car.cost}</h4>
-                             <small class="text-muted">${car.soldCount} artículos</small>
+                             <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
+                             <small class="text-muted">${product.soldCount} artículos</small>
                          </div>
-                         <p class="mb-1">${car.description}</p>
+                         <p class="mb-1">${product.description}</p>
                      </div>
                  </div>
              </div>
              `
              document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
         }
+        document.getElementById("textoP").innerHTML = `Estas viendo los productos de la categoría <b>${data.catName}.</b>`;
     })
     
 }
-showCarCat();
+
+showProducts();
