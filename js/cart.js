49f2a072de4d3b356b2a61be067d82cb8709d91c
todobanners <ecgb2007@gmail.com>
2022-11-19 02:15:52 -0300

    PRueba

--- js/cart.js
+++ js/cart.js
@@ -149,24 +149,8 @@ Array.from(forms).forEach(form => {
       }
 
     } else {
-      event.preventDefault() // Se agrega para evitar que se recargue la pagina y se pierda el mensaje.
+      //event.preventDefault() // Se agrega para evitar que se recargue la pagina y se pierda el mensaje.
       alert('Sus pedidos seran enviados segun lo seleccionado, gracias por comprar en e-mercado', 'success')
-      fetch("https://3000-cs-4d800d78-fa9c-40b3-b3a0-eb3052baf86e.cs-us-east1-vpcf.cloudshell.dev/compra", {
-    method: 'POST',
-    headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({
-        "calle": document.getElementById("calle").value,
-        "esquina": document.getElementById("esquina").value,
-        "numero": document.getElementById("numero").value
-    })
-})
-.then(respuesta => respuesta.json())
-.then(datos => {
-    console.log(datos);
-});
     }
 
     form.classList.add('was-validated')
